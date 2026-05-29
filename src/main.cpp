af46ce9584a2ca35907663d0093ea287797fe4f4
ALISSON ALMEIDA GOMES <52759068897@info.local>
2026-05-29 15:21:19 -0300

    Adicionei a logica de publicação JSON via MQTT/AWS

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <DHT.h>
-#include "DebugManager.h"
 #include "KY038.h"
+#include "DebugManager.h"
+#include "WiFiManager.h"
+#include "MqttManager.h"
 
-#define PIN_DHT 5 // pino escolhido
+#define PIN_DHT 40
 #define TIPO_DHT DHT22
-#define PIN_MIC 8
+#define PIN_MIC 20
+
+float temperatura;
+float umidade; 
+float ruido;
+int comandoAr;
+int alertaSom;
+bool eco;
+
+unsigned long ultimaPublicacao = 0;
+const unsigned long intervalo = 10000;
 
 void SensorUmidadeTemperatura();
 void configurarSensor();
 
-float temperatura;
-float umidade;
+const char  TOPICO_COMANDO[] = "sala09/analise/lado_A";
+
+void publicarDadosAnalise();
 
 DHT dht(PIN_DHT, TIPO_DHT);
 SENSOR sensor(PIN_MIC);
@@ -20,20 +34,32 @@ void setup()
 {
   configurarDebug();
   configurarSensor();
+  conectarWiFi();
+  configTime(10800, 0, "b.ntp.br");
+  configurarMQTT();
+  conectarMQTT();
 }
 
 void loop()
 {
-  //Serial.println("EU SOU GAY");
+  garantirWiFiConectado();
+  garantirMQTTConectado();
+  MQTTLoop();
   SensorUmidadeTemperatura();
+
+  if(millis() - ultimaPublicacao >= intervalo)
+  {
+    ultimaPublicacao = millis();
+    publicarDadosAnalise();
+  }
 }
 
 
 void SensorUmidadeTemperatura()
 {
   delay(1000);
-  umidade =  dht.readHumidity();
-  temperatura = dht.readTemperature();
+
+
 
   if(isnan(umidade) || isnan(temperatura))
   {
@@ -41,15 +67,10 @@ void SensorUmidadeTemperatura()
     debugInfo("Verifique a conexão e o pino definido");
     return;
   }
-
-   debugInfo("Temperatura: " + String(temperatura) + "°C");
-   debugInfo("Umidade: "+ String(umidade));
-   debugInfo("Coeficiente barulho: " + String(sensor.getPercentage(20)));
-   debugInfo("------------------------------");
-
-   return;
+  umidade =  dht.readHumidity();
+  temperatura = dht.readTemperature();
+  ruido = sensor.getPercentage(50);
 }
-
 
 void configurarSensor()
 {
@@ -59,4 +80,35 @@ void configurarSensor()
   dht.begin(); // inicializa o sensor DHT22
   debugInfo("Sensor inicializado");
   return;
+}
+
+void publicarDadosAnalise()
+{
+  unsigned long timestamp = time(nullptr);
+  JsonDocument doc;
+  JsonObject analise = doc["analise"].to<JsonObject>();
+
+  debugInfo("==============================");
+  debugInfo("Publicando dados de analise...");
+  debugInfo("==============================");
+  debugInfo("Timestamp: " + String(timestamp));
+  debugInfo("Temperatura: " + String(temperatura));
+  debugInfo("Umidade: " + String(umidade));
+  debugInfo("Ruido: " + String(ruido));
+  debugInfo("ComandoAr: " + String(comandoAr));
+  debugInfo("AlertaSom: " + String(alertaSom));
+  debugInfo("Eco: " + String(eco));
+
+  analise["timestamp"] = timestamp;
+  analise["temperatura"] = temperatura;
+  analise["umidade"] = umidade;
+  analise["ruido"] = ruido;
+  analise["comandoAr"] = comandoAr;
+  analise["alertaSom"] = alertaSom;
+  analise["eco"] = eco;
+
+  char buffer[256];
+
+  serializeJson(doc, buffer, sizeof(buffer));  // (JSON, onde vai ser escrito, tamanho maximo)
+  publicarMensagem(TOPICO_COMANDO, buffer);
 }
