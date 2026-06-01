2d15c5a61c8df809381fdd730707330af7ba4c43
KAEL FONTES ARAUJO <49797241831@info.local>
2026-06-01 15:23:42 -0300

    Tratamento de erros

--- src/main.cpp
+++ src/main.cpp
@@ -32,82 +32,82 @@ SENSOR sensor(PIN_MIC);
 
 void setup()
 {
-  configurarDebug();
-  configurarSensor();
-  conectarWiFi();
-  configTime(10800, 0, "b.ntp.br");
-  configurarMQTT();
-  conectarMQTT();
+    configurarDebug();
+    configurarSensor();
+    conectarWiFi();
+    configTime(10800, 0, "b.ntp.br");
+    configurarMQTT();
+    conectarMQTT();
 }
 
 void loop()
 {
-  garantirWiFiConectado();
-  garantirMQTTConectado();
-  MQTTLoop();
+    garantirWiFiConectado();
+    garantirMQTTConectado();
+    MQTTLoop();
 
-  if (millis() - ultimaPublicacao >= intervalo)
-  {
-    ruido = sensor.getPercentage(20);
-    if (SensorUmidadeTemperatura())
-      publicarDadosAnalise();
-    else
-      debugErro("Erro ao ler sensores, publicacão nao acontecerá");
-    ultimaPublicacao = millis();
-  }
+    if (millis() - ultimaPublicacao >= intervalo)
+    {
+        ruido = sensor.getPercentage(20);
+        if (SensorUmidadeTemperatura())
+            publicarDadosAnalise();
+        else
+            debugErro("Erro ao ler sensores, publicacão nao acontecerá");
+        ultimaPublicacao = millis();
+    }
 }
 
 bool SensorUmidadeTemperatura()
 {
-  umidade = dht.readHumidity();
-  temperatura = dht.readTemperature();
-  ruido = sensor.getPercentage(20);
-  return true;
-  if (isnan(umidade) || isnan(temperatura))
-  {
-    debugErro("Falha ao iniciar o sensor");
-    debugInfo("Verifique a conexão e o pino definido");
-    return false;
-  }
+    umidade = dht.readHumidity();
+    temperatura = dht.readTemperature();
+    ruido = sensor.getPercentage(20);
+    if (isnan(umidade) || isnan(temperatura))
+    {
+        debugErro("Falha ao iniciar o sensor");
+        debugInfo("Verifique a conexão e o pino definido");
+        return false;
+    }
+    return true;
 }
 
 void configurarSensor()
 {
 
-  debugInfo("==========Sensor DHT22==========");
+    debugInfo("==========Sensor DHT22==========");
 
-  dht.begin();
-  debugInfo("Sensor inicializado");
-  return;
+    dht.begin();
+    debugInfo("Sensor inicializado");
+    return;
 }
 
 void publicarDadosAnalise()
 {
-  unsigned long timestamp = time(nullptr);
-  JsonDocument doc;
-  JsonObject analise = doc["analise"].to<JsonObject>();
+    unsigned long timestamp = time(nullptr);
+    JsonDocument doc;
+    JsonObject analise = doc["analise"].to<JsonObject>();
 
-  debugInfo("==============================");
-  debugInfo("Publicando dados de analise...");
-  debugInfo("==============================");
-  debugInfo("Timestamp: " + String(timestamp));
-  debugInfo("Temperatura: " + String(temperatura));
-  debugInfo("Umidade: " + String(umidade));
-  debugInfo("Ruido: " + String(ruido));
-  debugInfo("ComandoAr: " + String(comandoAr));
-  debugInfo("AlertaSom: " + String(alertaSom));
-  debugInfo("Eco: " + String(eco));
+    debugInfo("==============================");
+    debugInfo("Publicando dados de analise...");
+    debugInfo("==============================");
+    debugInfo("Timestamp: " + String(timestamp));
+    debugInfo("Temperatura: " + String(temperatura));
+    debugInfo("Umidade: " + String(umidade));
+    debugInfo("Ruido: " + String(ruido));
+    debugInfo("ComandoAr: " + String(comandoAr));
+    debugInfo("AlertaSom: " + String(alertaSom));
+    debugInfo("Eco: " + String(eco));
 
-  analise["timestamp"] = timestamp;
-  analise["temperatura"] = temperatura;
-  analise["umidade"] = umidade;
-  analise["ruido"] = ruido;
-  analise["comandoAr"] = comandoAr;
-  analise["alertaSom"] = alertaSom;
-  analise["eco"] = eco;
+    analise["timestamp"] = timestamp;
+    analise["temperatura"] = temperatura;
+    analise["umidade"] = umidade;
+    analise["ruido"] = ruido;
+    analise["comandoAr"] = comandoAr;
+    analise["alertaSom"] = alertaSom;
+    analise["eco"] = eco;
 
-  char buffer[256];
+    char buffer[256];
 
-  serializeJson(doc, buffer, sizeof(buffer)); // (JSON, onde vai ser escrito, tamanho maximo)
-  publicarMensagem(TOPICO_COMANDO, buffer);
+    serializeJson(doc, buffer, sizeof(buffer)); // (JSON, onde vai ser escrito, tamanho maximo)
+    publicarMensagem(TOPICO_COMANDO, buffer);
 }
